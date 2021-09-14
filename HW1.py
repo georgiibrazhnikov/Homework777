542badae35151884c5afe0f7ff06b7197d154901
georgiibrazhnikov <90098904+georgiibrazhnikov@users.noreply.github.com>
2021-09-14 08:11:43 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,19 @@
+print('Введите a >>')
+a=input()
+print('Введите b >>')
+b=input()
+print('Введите c >>')
+c=input()
+print('Введите d >>')
+d=input()
+print('Введите f >>')
+f=input()
+if f-d==0:
+    print('Делить на ноль нельзя!')  
+else:
+    g=(a * b - c) / (f - d)
+print(g)
+
+
+
+
---
+++ HW3.py
@@ -0,0 +1,7 @@
+print('Введите X,Y,Z >>')
+X=input()
+Y=input()
+Z=input()
+K=X*X+Y*Y+Z*Z
+V=math.sqrt(K)
+print(K)
