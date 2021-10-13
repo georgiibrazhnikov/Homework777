70c45bffe500bcd3d73341124f89098f213d5ebe
georgiibrazhnikov <90098904+georgiibrazhnikov@users.noreply.github.com>
2021-10-13 10:37:10 +0300

    Update HW1.5.py

--- HW1.5.py
+++ HW1.5.py
@@ -1,12 +1,12 @@
 x = int(input())
 y = int(input())
 if x > 0 and y > 0:
-        print('В первой четверти') 
+        print('В первой четверти')
 elif x < 0 and y > 0:
         print('Во второй четверти')
 elif x < 0 and y < 0:
-        print('В третьей четверти') 
+        print('В третьей четверти')
 elif x > 0 and y < 0:
         print('В четвёртой четверти')
-else: 
+else:
         print('На оси')
