18c075ceb96ecb4d3182c2095d8f412188d79b77
georgiibrazhnikov <90098904+georgiibrazhnikov@users.noreply.github.com>
2021-10-30 08:53:08 +0300

    Update HW2.7.py

--- HW2.7.py
+++ HW2.7.py
@@ -1,13 +1,12 @@
 from random import randint
 
 k = randint(0, 100)
-print('Загадайте число от 0 до 100 >>')
-x = int(input())
+x = int(input('Загадайте число от 0 до 100 >> '))
 while k != x:
     print('Вы не угадали :(')
     if k > x:
         print('Загаданное число больше!')
     elif k < x:
         print('Загаданное число меньше!')
-    x = int(input())
+    x = int(input('Загадайте число ещё раз>> '))
 print('Вы угадали число!')
