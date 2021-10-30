a5ba1c378bc6ea810f4fff3d3b8a81f1b72187ec
georgiibrazhnikov <90098904+georgiibrazhnikov@users.noreply.github.com>
2021-10-30 08:43:32 +0300

    Add files via upload

---
+++ HW2.6.py
@@ -0,0 +1,9 @@
+print('Введите строку >>')
+l = input()
+a = []
+for i in l:
+    if i.isdigit():
+        a.append(i)
+print('Введите k >>')
+k = int(input())
+print(k, '-ая цифра в строке', a[k - 1])
