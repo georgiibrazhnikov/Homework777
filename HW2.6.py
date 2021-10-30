c15ba0cab4cf36dc39e3da5b28f9d9357245fadc
georgiibrazhnikov <90098904+georgiibrazhnikov@users.noreply.github.com>
2021-10-30 08:42:56 +0300

    Delete HW2.6.py

--- HW2.6.py
+++
@@ -1,9 +0,0 @@
-print('Введите строку >>')
-l = list(input())
-a = []
-for i in l:
-    if i.isdigit():
-        a.append(i)
-print('Введите k >>')
-k = int(input())
-print(k, '-ая цифра в строке', a[k-1])
