f581250908092780016ee2b5e2439a65f36a52c1
georgiibrazhnikov <90098904+georgiibrazhnikov@users.noreply.github.com>
2022-02-26 09:14:55 +0300

    Update HW2.1.py

--- HW2.1.py
+++ HW2.1.py
@@ -1,14 +1,18 @@
 class Fraction:
 
-    def __init__(self, nom: int = 1, denom: int = 0):
+    def __init__(self, nom: int = 0, denom: int = 1) -> None:
         self.nom = nom
         self.denom = denom
 
-    def vvod(self):
+    def vvod(self) -> None:
         self.nom = int(input('Введите числитель>> '))
         self.denom = int(input('Введите знаменатель>> '))
         if self.denom == 0:
-            print('Знаменатель не должен равняться нулю.')
+            raise ZeroDivisionError
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.ch}/{self.zn}"
+
+
+a1 = Fraction()
+print(a1)
